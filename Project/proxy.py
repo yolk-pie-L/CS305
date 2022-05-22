b4cfa557b3389acdbe81170fb38641851c2f668d
yolk-pie-L <91405207+yolk-pie-L@users.noreply.github.com>
2022-05-22 21:12:45 +0800

    change method

--- Project/proxy.py
+++ Project/proxy.py
@@ -1,12 +1,42 @@
-from urllib import request
-from flask import Flask, Response,Request
+import math
+from multiprocessing.dummy import active_children
+import socket
+import sys
+import threading
+import time
+from threading import Thread
 
-app = Flask(__name__)
+from dns.query import udp
+from dns import *
+import dns
+import argparse
 
+"""
+This framework is just a reference for beginning. Feel free to change it!
+Have a good luck!
+"""
 
-@app.route('/example')
-def simple():
-    return Response(request.get('http://www.example.com'))
+def recv(s):
+    """
+    recevie the request passed to proxy.
+    """
+
+def send(s):
+    """
+    send the response here.
+    """  
+
+def exit():
+    """
+    you should provide a way to exit your proxy well.
+    """
+
+def accept(PORT):
+    """
+    you should bind the ip of your socket to 0.0.0.0 to make the proxy work well
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("0.0.0.0", PORT))
 
 
 def modify_request(message):
@@ -18,8 +48,10 @@ def modify_request(message):
 
 def request_dns():
     """
-    Request dns server here.
+    Request dns server here. Specify the domain name as you want.
     """
+    query = message.make_query("xxx", dns.rdatatype.A,
+                                        dns.rdataclass.IN)
 
 def calculate_throughput():
     """
@@ -27,5 +59,43 @@ def calculate_throughput():
     """
 
 
+class Proxy():
+    """
+    The class is used to manage connections from clients.
+    """
+    def __init__(self):
+        self.connection = None
+        self.send_buffer = None
+        self.receive_buffer = None
+        """
+        Add field as you want
+        """
+
+
+class Connection():
+    def __init__(self, conn, address):
+        self.conn = conn
+        self.address = address
+        """
+        Add field as you want
+        """
+    
+
+
 if __name__ == '__main__':
-    app.run(port=8999)
+   
+    """
+    Parse command varibles first.
+    """
+    parser = argparse.ArgumentParser(description='start proxying......')
+    parser.add_argument('-p', '--port', required=True,
+                            help='listening port for proxy.')
+    args = parser.parse_args()
+
+    """
+    Start your proxy.
+    """
+
+    accept(args.port)
+    
+

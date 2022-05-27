0dfb9c1258c73a14401ff6b9d5c2a9ee78818822
Falken Sakurai <58453886+KagaJiankui@users.noreply.github.com>
2022-05-27 22:08:52 +0800

    Fix bug when using dnspython
    write a UDP client on hand because I don't know how to write a multi-thread WS server for multiple clients

--- Project/testdns.py
+++ Project/testdns.py
@@ -1,11 +1,16 @@
+import socket
 from dns import resolver, rdatatype
+import dnslib
+from dnslib.dns import QTYPE, RR
 
 
 def request_dns(Port: int) -> int:
+    # BUG: dns.resolver.NoNameservers: All nameservers failed to answer the query localhost/index.html. IN TXT: Server 127.0.0.1 UDP port 53 answered A DNS query response does not respond to the question asked.
     dns_resolver = resolver.Resolver()
     dns_resolver.nameservers = ["127.0.0.1"]
     dns_resolver.port = Port
-    result = dns_resolver.resolve(qname="localhost/index.html", rdtype=rdatatype.TXT)
+    result = dns_resolver.resolve(qname="localhost/index.html")
+    print(str(result))
     web_port = 0
     for i in result.response.answer:
         for j in i.items:
@@ -14,4 +19,51 @@ def request_dns(Port: int) -> int:
     return web_port
 
 
-print("DNS Port: ", request_dns(53))
+def request_dns_alt(Port: int, Type: int) -> int:
+    """DNS Client For Port Acquisition
+
+    Args:
+        Port (int): Target port number of the DNS server that provides port information
+        Type (int): Type of the DNS RR that carries the port information
+
+    Returns:
+        int: Acquisited port number
+    """
+    # USE THIS ONE
+    dns_request = dnslib.DNSRecord.question("localhost/index.html", "TXT")
+    data = dns_request.pack()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.sendto(data, ("127.0.0.1", Port))
+    result, addr = server_socket.recvfrom(1024)
+    answer = dnslib.DNSRecord.parse(result)
+    ports = 0
+    for resources in answer.rr:
+        if resources.rtype == Type:
+            ports = resources.rdata.data[0]
+    server_socket.close()
+    return ports
+
+
+def request_dns_test_loop(Port: int, Type: int):
+    # FOR TEST ONLY
+    dns_request = dnslib.DNSRecord.question("localhost/index.html", "TXT")
+    data = dns_request.pack()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    flag = "y"
+    try:
+        while flag == "y":
+            server_socket.sendto(data, ("127.0.0.1", Port))
+            result, addr = server_socket.recvfrom(1024)
+            answer = dnslib.DNSRecord.parse(result)
+            ports = []
+            for resources in answer.rr:
+                if resources.rtype == dnslib.QTYPE[Type]:
+                    ports.append(resources.rdata)
+            print(ports)
+            flag = input("Do you want to continue? (y/n) ")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        server_socket.close()
+
+
+print("DNS Port: " + str(request_dns_alt(53, dnslib.QTYPE.TXT)))

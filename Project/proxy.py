4fbc183686cfb951a1014d20aa190c4a17cbd6b5
yolk-pie-L <91405207+yolk-pie-L@users.noreply.github.com>
2022-05-27 20:22:03 +0800

    Update proxy.py

--- Project/proxy.py
+++ Project/proxy.py
@@ -1,239 +1,180 @@
-import math
-import re
-from socket import socket, AF_INET, SOCK_STREAM, error
-import sys
-import threading
+#!/usr/bin/env python
+from socket import socket, AF_INET, SOCK_STREAM
+import select
 import time
-from threading import Thread
-from xml.dom.minidom import parseString
-
-from dns.query import udp
-from dns import *
-import dns
+import re
+import sys
+import thread
+from multiprocessing import Process
 import argparse
 
-bbbf4m = ""  # place the big_buck_bunny.f4m file contents here，判断如果不为空就不用存了（相当于缓存起来）
-bitrates = []  # 从bbbf4m解析得到的bitrates
 
+class Proxy:
+    def __init__(self, soc, alpha, fake_ip, server_ip, dns_port, webserver_port):
+        print('init: accepting client')
+        self.client, _ = soc.accept()
+        self.target = None
+        self.request_url = server_ip
+        self.fake_ip = (fake_ip, 0)
+        self.send_count = 0
+        self.recv_count = 0
+        self.br = 10
+        self.a = alpha
+        self.dns_port = dns_port
+        self.webserver_port = webserver_port
 
-def recv(req, s, sock):
-    try:
-        while '\r\n\r\n' not in req:
-            req += sock.recv(1).decode()  # receive HTTP header
-        return req
-    except error:
-        print('Oooops! Error happened when recv : ' + str(error))
-        s.stop()
+    def getClientRequest(self):
+        # print('receving from client')
+        request = self.client.recv(4096)
+        if request is None:
+            return None
+        return request
 
+    def connectServer(self, request):
+        self.target = socket(AF_INET, SOCK_STREAM)
+        # print('connecting to server')
+        self.target.bind(self.fake_ip)  # bind socket to fake ip
+        if self.webserver_port:
+            self.target.connect((self.request_url, self.webserver_port))
+        else:
+            webserver_port = self.dnsRequest()
+            self.target.connect((self.request_url, webserver_port))
+        # print('sending message to server')
+        self.target.send(request)
+        self.communicating()
+        
+        
+    def dnsRequest(self):
+        raise NotImplementedError
+        
 
-def send(req, s, sock):
-    try:
-        sock.send(req.encode(encoding='gbk'))
-    except error:
-        print('Oooops! Error happened when send : ' + str(error))
-        s.stop()
+    def chooseBitrate(self, throughput):
+        global bitrate
+        length = len(bitrate)
+        for i in range(length):
+            if throughput / 1.5 < bitrate[i]:
+                if i == 0:
+                    return bitrate[0]
+                else:
+                    return bitrate[i - 1]
+        return bitrate[length - 1]
 
+    def communicating(self):
+        global bitrate
+        # patterns for finding corresponding information
+        pat_big_buck = re.compile(b'.f4m')
+        pat_length = re.compile(b'Content-Length: .\w+')
+        pat_response = re.compile(b'Connection:')
+        pat_name = re.compile(b'/vod/.*Frag[0-9]*')
+        pat_bitrate = re.compile(b'bitrate="[0-9]*"')
+        pat_change = re.compile(b'/vod/[0-9]*')
+        # initialize parameters 
+        inputs = [self.client, self.target]
+        buff_size = 4096
+        cur_count = 0
+        ts = time.time()
+        chunkname = 'Not a chunk'
+        while True:
+            readable, writeable, errs = select.select(inputs, [], inputs, 20)
+            if errs:
+                break
+            for soc in readable:
+                # receive data and check whether it is from client or server
+                data = soc.recv(buff_size)
+                if data:
+                    if soc is self.client:
+                        # ts = time.time() # if start timer when receving request, start ts here.
+                        self.send_count += 1
+                        result = re.search(pat_big_buck, data, flags=0)
+                        if result != None:
+                            data.decode()
+                            real_data = data
+                            data = data.replace('.f4m', '_nolist.f4m')  # request no_list.f4m instead of original .f4m
+                            data.encode()
+                            real_data.encode()
+                            self.target.send(real_data)
+                            manifest = self.target.recv(409600)
+                            bitrate_list = re.findall(pat_bitrate, manifest)
+                            for i in bitrate_list:
+                                print(int(i.split('"')[1]))
+                                bitrate.append(int(i.split('"')[1]))  # add received bittrates into a list
+                        result = re.search(pat_name, data)
+                        if result != None:
+                            data = re.sub(pat_change, '/vod/' + str(self.br), data)
+                            chunkname = result.group(0)
+                            chunkname = re.sub(r'/vod/[0-9]*', '/vod/' + str(self.br), chunkname)
+                        else:
+                            chunkname = 'Not a chunk'
+                        self.target.send(data)
+                    if soc is self.target:
+                        # use content length and packet count to find the last packet's postion
+                        result = re.search(pat_length, data, flags=0)
+                        if result != None:
+                            length = float(result.group(0)[16:])
+                            cur_count = 1
+                        else:
+                            cur_count += 1
+                        self.client.send(data)
+                        # locate the last packet
+                        if cur_count * buff_size > length:
+                            cur_count = 1
+                            if self.recv_count > 0:
+                                tf = time.time()
+                                dur = tf - ts
+                                thr = 8 * length / (dur) / 1024
+                                avg_thr = self.a * thr + (1 - self.a) * t_old
+                                ts = time.time()  # start ts here to have a better duration estimation
+                                t_old = avg_thr
+                                if chunkname != 'Not a chunk':
+                                    log.write('%d %f %d %.1f %d %s %s\n' % (
+                                    tf, dur, thr, avg_thr, self.br, self.request_url, chunkname))
+                                    self.br = self.chooseBitrate(avg_thr)
+                            else:
+                                t_old = 0
+                            self.recv_count += 1
+                else:
+                    break
+        self.client.close()
+        self.target.close()
+        self.log.close()
 
-def exit():
-    """
-    you should provide a way to exit your proxy well.
-    """
-
-
-def accept(PORT: int):
-    """
-    you should bind the ip of your socket to 0.0.0.0 to make the proxy work well
-    """
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.bind(("0.0.0.0", PORT))
-    return sock
-
-
-def modify_request(message):
-    """
-    Here you should change the requested bit rate according to your computation of throughput.
-    And if the request is for big_buck_bunny.f4m, you should instead request big_buck_bunny_nolist.f4m
-    for client and leave big_buck_bunny.f4m for the use in proxy.
-    """
-    index_point = message.find('.f4m')
-    message = message[0:index_point] + '_nolist' + message[index_point:]
-    return message
-
-
-def request_dns(Port: int) -> int:
-    dns_resolver = resolver.Resolver()
-    dns_resolver.nameservers = ['127.0.0.1']
-    dns_resolver.port = Port
-    result = dns_resolver.resolve(qname='localhost/index.html', rdtype=rdatatype.A)
-    web_port = 0
-    for i in result.response.answer:
-        for j in i.items:
-            web_port = int(j.to_text())
-    return web_port
-
-
-def first_chunk_req() -> str:
-    doc = parseString(bbbf4m)
-    collection = doc.documentElement
-    media = collection.getElementsByTagName("media")
-    for m in media:
-        bitrates.append(int(m.attributes['bitrate'].value))
-    bitrates.sort(reverse=True)
-    return str(bitrates[-1])
-
-
-def bitrate_adaptation(B: int, ts: float, tf: float, T_old: float, a: float, f, serverport: int,
-                       chunkname: str) -> tuple:
-    """
-    calculate throughput, choose appropriate bitrate, and write to log
-    Parameters:
-    B: the length of the video trunk in bytes, ts: start time, tf: end time, Told: previous throughput, \
-        a: alpha, f: fileIO, serverport: web server port, chunkname: Seg%d-Frag%d, e.g. Seg1-Frag2
-    Returns:
-    a tuple with the form (current_throughput: float, bitrate_selection: str)
-    """
-    # calculate throughput
-    T_new = B * 8 / (1024 * (tf - ts))
-    T_avg = a * T_new + (1 - a) * T_old
-    # bitrate selection
-    if bitrates:
-        doc = parseString(bbbf4m)
-        collection = doc.documentElement
-        media = collection.getElementsByTagName("media")
-        for m in media:
-            bitrates.append(int(m.attributes['bitrate'].value))
-        bitrates.sort(reverse=True)
-    T_choose = T_avg / 1.5
-    bitchoose=''
-    for b in bitrates:
-        if b <= T_choose:
-            bitchoose = str(b)
-            break
-    # write to log
-    chunkname = bitchoose + chunkname
-    f.write(str(int(ts)) + ' ' + str(tf - ts) + ' ' + str(T_new) + ' ' + str(T_avg) + \
-            bitchoose + ' ' + str(serverport) + ' ' + chunkname + '\n')
-    return T_avg, bitchoose
-
-
-class Proxy(threading.Thread):
-    def __init__(self, sock, log_file, alpha, dns_server_port, web_server_port):
-        threading.Thread.__init__(self)
-        self.sock = sock # browser connection
-        self.log_file = log_file
-        self.alpha = alpha
-        self.web_server_port = web_server_port
-        self.dns_server_port = dns_server_port
-        self.server_socket = None
-        self.count = 0
-        self.thread_stop = False
-
-    def stop(self):
-        self.thread_stop = True
-        self.sock.close()
-        self.server_socket.close()
-
-    def run(self) -> None:
-        global bbbf4m
-        t_current = 10.0
-        bit_choose = '10'
-        while not self.thread_stop:
-            body_len = 0
-            # Receive HTTP request from Browser
-            browser_req = ''
-            browser_req = recv(browser_req, self, self.sock)
-            web_server_port = self.web_server_port
-            if self.web_server_port is None:
-                web_server_port = request_dns(self.dns_server_port)
-            self.server_socket = socket(AF_INET, SOCK_STREAM)
-            self.server_socket.bind(("127.0.0.1", 0))
-            self.server_socket.connect(("0.0.0.0", web_server_port))
-            # Check if request is for manifest file
-            if '.f4m' in browser_req:
-                # Send HTTP request to server
-                if len(bbbf4m)==0:
-                    send(browser_req, self, self.server_socket)
-                    # Receive HTTP header of response from Server
-                    server_resp = ''
-                    server_resp = recv(server_resp, self, self.server_socket)
-                    # Reseive HTTP body of response
-                    proxy_len = int(re.search('(?<=Content-Length: )\d+', server_resp).group(0))
-                    recvcount = 0
-                    try:
-                        while recvcount < proxy_len:
-                            bbbf4m += self.server_socket.recv(1).decode()
-                            recvcount += 1
-                    except error:
-                        print("Oooops! Error happened when receiving .f4m : ")
-                        self.stop()
-                # Modify browser response to be for browser manifest file
-                browser_req = modify_request(browser_req)
-            # check if request is for a chunk
-            chunk_req = re.search('[\d]*Seg[\d]*-Frag[\d]*', browser_req)
-            if chunk_req:
-                # modify chunk request to be for same chunk, but of appropriate bitrate
-                index_point_start = browser_req.find(chunk_req.group(0))
-                index_point_end = browser_req.find('Seg')
-                if self.count == 0:
-                    bit_choose = first_chunk_req()
-                    self.count += 1
-                browser_req = browser_req[0:index_point_start] + bit_choose + browser_req[index_point_end:]
-
-            t_s = time.time()
-            # Send HTTP request to server
-            send(browser_req, self, self.server_socket)
-            # Receive HTTP header of response from server
-            server_resp = ''
-            server_resp = recv(server_resp, self, self.server_socket)
-            # Receive HTTP body of response from server
-            if 'Content-Length: ' in server_resp:
-                body_len = int(re.search('(?<=Content-Length: )\d+', server_resp).group(0))
-                recvcount = 0
-                try:
-                    while recvcount < body_len:
-                        server_resp += self.server_socket.recv(1).decode()
-                        recvcount += 1
-                except error:
-                    print("Oooops! Error happened when receive : "+server_resp)
-                    self.stop()
-            t_f = time.time()
-            chunkname=''
-            if chunk_req:
-                up_to_chunkname = re.match('GET [\S]*', browser_req).group(0)
-                chunkname = up_to_chunkname[4:]
-            t_current, bit_choose = bitrate_adaptation(body_len, t_s, t_f, t_current, self.alpha, self.log_file, web_server_port,
-                                     chunkname)
-            # Send HTTP response to Browser
-            send(server_resp, self, self.sock)
+    def run(self):
+        request = self.getClientRequest()
+        if request:
+            self.connectServer(request)
 
 
 if __name__ == '__main__':
 
-    # 解析命令行参数
+    # parse command line args
     parser = argparse.ArgumentParser(description='start proxying......')
+
     parser.add_argument('-l', '--logfile', required=True, help='log file path')
     parser.add_argument('-a', '--alpha', required=True, type=float, help='set value of alpha')
     parser.add_argument('-p', '--port', required=True, type=int, help='listening port for proxy.')
     parser.add_argument('-P', '--Port', required=True, type=int, help='DNS server port')
     parser.add_argument('-s', '--webserverport', required=False, type=int, help='default webserver port')
-    parser.add_argument('-e', '--exit', required=False, choices=['exit', 'start'])
 
     args = parser.parse_args()
 
-    proxys = []
-    if args.exit == 'exit':
-        for p in proxys:
-            p.stop()
-        sys.exit(0)
+    log = open(args.logfile, 'w')
+    a = args.alpha
+    listen_port = args.port
+    dns_port = args.Port
+    webserver_port = args.webserverport
 
-    f = open(args.logfile, 'w')
+    fake_ip = '0.0.0.0'
+    server_ip = '127.0.0.1'
 
-    browser_socket = accept(args.port)  # browser listen
-    browser_socket.listen(1000)
+    proxySocket = socket(AF_INET, SOCK_STREAM)
+    proxySocket.bind(('', int(listen_port)))
+    proxySocket.listen(10)
+    bitrate = []
+    print('start')
 
     while True:
-        # accept browser request and create browser_connection_socket
-        sock, addr = browser_socket.accept()  # browser connect
-        proxy = Proxy(sock, f, args.alpha, args.Port, args.webserverport)
-        proxys.append(proxy)
-        proxy.start()
+        try:
+            # a new thread for each connection
+            thread.start_new_thread(Proxy(proxySocket, float(a), fake_ip, server_ip, dns_port, webserver_port).run, ())
+        except Exception as e:
+            print(e)

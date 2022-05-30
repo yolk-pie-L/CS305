2de189f3eae6d11d7dea4fc357ae8cf20e6d6334
yolk-pie-L <91405207+yolk-pie-L@users.noreply.github.com>
2022-05-30 22:13:41 +0800

    Update proxy.py

--- Project/proxy.py
+++ Project/proxy.py
@@ -31,19 +31,13 @@ class Proxy:
         self.target = None
         self.webserver_ip = '127.0.0.1'
         self.proxy_ip = ('0.0.0.0', 0)
-        self.send_count = 0
-        self.recv_count = 0
         self.br = 10
+        self.thr = 0
         self.a = alpha
         self.dns_port = dns_port
         self.webserver_port = webserver_port
 
-    def getClientRequest(self):
-        # print('receving from client')
-        request = self.client.recv(4096)
-        return request
-
-    def connectServer(self, request):
+    def connectServer(self):
         self.target = socket(AF_INET, SOCK_STREAM)
         # print('connecting to server')
         self.target.bind(self.proxy_ip)  # bind socket to fake ip
@@ -54,7 +48,6 @@ class Proxy:
             print("webserver_port" + self.webserver_port)
             self.target.connect((self.webserver_ip, int(self.webserver_port)))
         # print('sending message to server')
-        self.target.send(request)
         self.communicating()
 
 
@@ -65,91 +58,102 @@ class Proxy:
                 return b
         return bitrate[-1]
 
+    def recvFromApache(self):
+        buff_size = 1024
+        data = self.target.recv(buff_size)
+        if len(data) < buff_size:
+            return data
+        while True:
+            tempt = self.target.recv(buff_size)
+            data += tempt
+            if len(tempt) < buff_size:
+                break
+        print('apache length'+str(len(data)))
+        # buff_size = 8192
+        # pat_length = re.compile(b'Content-Length: .\w+')
+        # data = self.target.recv(buff_size)
+        # result = re.search(pat_length, data, flags=0)
+        # cur_count = 0
+        # if result != None:
+        #     length = int(result.group(0)[16:])
+        #     print("apache length"+str(length))
+        #     cur_count = 1
+        #     while cur_count * buff_size < length:
+        #         data += self.target.recv(buff_size)
+        #         cur_count = cur_count + 1
+        # else:
+            # print("no length field")
+        return data
+
+
+    def sendToClient(self, data):
+        self.client.sendall(data)
+
     def communicating(self):
         global bitrate
         # patterns for finding corresponding information
         pat_bbb = re.compile(b'.f4m')
-        pat_length = re.compile(b'Content-Length: .\w+')
         pat_name = re.compile(b'Seg[0-9]*-Frag[0-9]*')
         pat_bitrate = re.compile(b'bitrate="[0-9]*"')
+
         # initialize parameters
-        inputs = [self.client, self.target]
         buff_size = 4096
-        cur_count = 0
         ts = time.time()
         chunkname = ''
         while True:
-            readable, writeable, errs = select.select(inputs, [], inputs, 20)
-            if errs:
-                break
-            for soc in readable:
-                # receive data and check whether it is from client or server
-                data = soc.recv(buff_size)
-                if data:
-                    if soc is self.client:
-                        # ts = time.time() # if start timer when receving request, start ts here.
-                        self.send_count += 1
-                        result = re.search(pat_bbb, data, flags=0)
-                        if result != None: # if the request is for .f4m
-                            data.decode()
-                            real_data = data
-                            data = data.replace('.f4m', '_nolist.f4m')  # request no_list.f4m instead of original .f4m
-                            data.encode()
-                            real_data.encode()
-                            self.target.send(real_data)
-                            manifest = self.target.recv(409600)
-                            bitrate_list = re.findall(pat_bitrate, manifest)
-                            for i in bitrate_list:
-                                bitrate.append(int(i.split('"')[1]))  # add received bitrates into a list
-                            bitrate.sort(reverse=True)
-                        result = re.search(pat_name, data)
-                        if result != None: # if the request is for a chunk
-                            data.decode()
-                            data = re.sub(r'[0-9]*Seg', str(self.br) + 'Seg', data)
-                            data.encode()
-                            chunkname = result.group(0)
-                        else:
-                            chunkname = ''
+            data = self.client.recv(buff_size)
+            result = re.search(pat_bbb, data, flags=0)
+            if result != None: # if the request is for .f4m
+                data.decode()
+                real_data = data
+                print("request f4m")
+                data = data.replace('.f4m', '_nolist.f4m')  # request no_list.f4m instead of original .f4m
+                data.encode()
+                real_data.encode()
+                self.target.send(real_data)
+                manifest = self.recvFromApache()
+                bitrate_list = re.findall(pat_bitrate, manifest)
+                for i in bitrate_list:
+                    bitrate.append(int(i.split('"')[1]))  # add received bitrates into a list
+                bitrate.sort(reverse=True)
+                self.target.send(data)
+                fakefile = self.recvFromApache()
+                self.sendToClient(fakefile)
+                continue
 
-                        self.target.send(data)
-                    if soc is self.target:
-                        tf = time.time()
-                        # use content length and packet count to find the last packet's postion
-                        result = re.search(pat_length, data, flags=0)
-                        if result != None:
-                            length = float(result.group(0)[16:])
-                            cur_count = 1
-                        else:
-                            cur_count += 1
-                        self.client.send(data)
-                        # locate the last packet
-                        if cur_count * buff_size > length:
-                            cur_count = 1
-                            if self.recv_count > 0:
-                                dur = tf - ts
-                                thr = 8 * length / (dur) / 1024
-                                avg_thr = self.a * thr + (1 - self.a) * t_old
-                                ts = time.time()  # start ts here to have a better duration estimation
-                                t_old = avg_thr
-                                print("chunkname"+"("+chunkname+")")
-                                if chunkname != '':
-                                    log.write('%d %f %d %.1f %d %s %s %s\n' % (
-                                        ts, dur, thr, avg_thr, self.br, self.webserver_ip, self.webserver_port,
-                                        chunkname))
-                                    self.br = self.chooseBitrate(avg_thr)
-                            else:
-                                t_old = 0
-                            self.recv_count += 1
-                else:
-                    break
-        self.client.close()
-        self.target.close()
-        log.close()
+            result = re.search(pat_name, data)
+            if result != None: # if the request is for a chunk
+                ts = time.time()
+                data.decode()
+                data = re.sub(r'[0-9]*Seg', str(self.br) + 'Seg', data)
+                print(data)
+                data.encode()
+                chunkname = result.group(0)
+                self.target.send(data)
+                recv_data = self.recvFromApache()
+                tf = time.time()
+                self.sendToClient(recv_data)
+                dur = tf - ts
+                B = len(recv_data)
+                t_new = B * 8 / dur / 1024
+                self.thr = t_new * self.a + (1 - a) * self.thr
+                self.br = self.chooseBitrate(self.thr)
+                log.write('%d %f %d %.1f %d %s %s %s\n' % (
+                    ts, dur, t_new, self.thr, self.br, self.webserver_ip, self.webserver_port, chunkname))
+                continue
+
+             # if the request is for other files
+            self.target.send(data)
+            recv_data = self.recvFromApache()
+            self.sendToClient(recv_data)
+
+            
+        # self.client.close()
+        # self.target.close()
+        # log.close()
 
     def run(self):
-        request = self.getClientRequest()
-        if request:
-            self.connectServer(request)
+        self.connectServer()
 
 
 if __name__ == '__main__':
